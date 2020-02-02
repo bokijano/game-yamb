7438991e0ce7d93e21ef9f995218848d7fc3b0d0
Bojan Krstic <krstic.bojan80@gmail.com>
2020-02-02 13:55:47 +0100

    Updates

---
+++ precache-manifest.d2e95b84b1eea08cad01eaabc3fde9c3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "9db14af6d3b93b42778c",
+    "url": "/game-yamb/static/css/main.8f0d6a4e.chunk.css"
+  },
+  {
+    "revision": "9db14af6d3b93b42778c",
+    "url": "/game-yamb/static/js/main.9db14af6.chunk.js"
+  },
+  {
+    "revision": "26cc8bd4db998a08362e",
+    "url": "/game-yamb/static/js/runtime~main.26cc8bd4.js"
+  },
+  {
+    "revision": "09079fb0d33bbf107cc7",
+    "url": "/game-yamb/static/js/2.09079fb0.chunk.js"
+  },
+  {
+    "revision": "96799f87b84fd9909b70b6a1ccfd2e87",
+    "url": "/game-yamb/static/media/dice1.96799f87.png"
+  },
+  {
+    "revision": "eea2daaded334676ba50863d42ffa2a2",
+    "url": "/game-yamb/static/media/dice2.eea2daad.png"
+  },
+  {
+    "revision": "b2df59e3c6d360db8840a681ed8f974e",
+    "url": "/game-yamb/static/media/dice3.b2df59e3.png"
+  },
+  {
+    "revision": "9a0c312c11adb36d51ce7150c07038b5",
+    "url": "/game-yamb/static/media/dice4.9a0c312c.png"
+  },
+  {
+    "revision": "72db359704fdda872cd7be1c3ec31a2a",
+    "url": "/game-yamb/static/media/dice5.72db3597.png"
+  },
+  {
+    "revision": "428963c97054c088aa14e01cf84cb184",
+    "url": "/game-yamb/static/media/dice6.428963c9.png"
+  },
+  {
+    "revision": "e1116c92b116eda27bf53a6fb44d60c2",
+    "url": "/game-yamb/index.html"
+  }
+];
